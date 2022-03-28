030f600e7ea316751447cecc58fe3b9e1b193050
arif <hossaininfo@ymail.com>
2022-03-28 13:21:04 +0200

    get all student method created

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,30 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/student")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    private StudentService studentService;
+
+    @GET
+    @Path("")
+    public Response getAllStudent() {
+        List<Student> students = studentService.getAllStudent();
+        return Response.ok(students).build();
+    }
 
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,24 @@
 package se.iths.service;
 
 
+import se.iths.entity.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+@Transactional
 public class StudentService {
 
+    @PersistenceContext
+    private EntityManager entityManager;
 
+    public List<Student> getAllStudent() {
+
+        List<Student> students = entityManager
+                .createQuery("select s from Student s", Student.class)
+                .getResultList();
+
+        return students;
+    }
 }
