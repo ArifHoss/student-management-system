af4fffafd9993f5986cd73bd33b20908e1132d0d
arif <hossaininfo@ymail.com>
2022-03-30 18:07:02 +0200

    added exception to @PUT request method updateAllStudent

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -92,6 +92,28 @@ public class StudentRest {
     @PUT
     @Path("")
     public Response updateAllStudent(Student student) {
+        String firstName = student.getFirstName();
+        String lastName = student.getLastName();
+        String email = student.getEmail();
+        List<Student> existEmail = studentService.existByEmail();
+
+        if(firstName ==null){
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("FIRSTNAME_CANNOT_BE_NULL").build());
+        }
+        if(lastName ==null){
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("LASTNAME_CANNOT_BE_NULL").build());
+        }
+
+        if(email==null){
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("EMAIL_CANNOT_BE_NULL").build());
+        }
+        if (existEmail.contains(email)) {
+            throw new WebApplicationException(Response.status(Response.Status.CONFLICT)
+                    .entity("EMAIL_" + email + "_ALREADY_EXIST!TRY_WITH_ANOTHER_EMAIL").build());
+        }
         studentService.updateAllStudent(student);
         return Response.ok(student).build();
     }
